ac439cfbe3f96a4a521f5c4cef952cda59cfa85f
erikbjohn <erikbjohn@gmail.com>
2018-10-04 16:02:26 -0500

    district maps clean up

--- R/get_district_maps.R
+++ R/get_district_maps.R
@@ -107,11 +107,50 @@ get_district_maps <- function(){
   shp_15_16@data <- data.table(shp_15_16@data, YEAR=2016)
   shp_15_16@data <- shp_15_16@data[, shp_LEA:=GEOID]
   proj4string(shp_15_16) <- proj_env
-  district_maps <- list(shp_89_90, shp_95_96, shp_97_98,
+  l_district_maps <- list(shp_89_90, shp_95_96, shp_97_98,
                           shp_99_00, shp_01_02, shp_03_04,
                           shp_05_06, shp_07_08, shp_09_10, 
                           shp_11_12, shp_13_14, shp_15_16)
-  saveRDS(district_maps, file=district_maps_location)
+  
+  l_districts <- vector('list', length=25)
+  l_districts[[1]] <- list(sYear='8990', numYear = 1989, map=l_district_maps[[1]], iMap = 1)
+  l_districts[[2]] <- list(sYear='8990', numYear = 1990, map=l_district_maps[[1]], iMap = 1)
+  l_districts[[3]] <- list(sYear='8990', numYear = 1991, map=l_district_maps[[1]], iMap = 1)
+  l_districts[[4]] <- list(sYear='8990', numYear = 1992, map=l_district_maps[[1]], iMap = 1)
+  l_districts[[5]] <- list(sYear='8990', numYear = 1993, map=l_district_maps[[1]], iMap = 1)
+  l_districts[[6]] <- list(sYear='8990', numYear = 1994, map=l_district_maps[[1]], iMap = 1)
+  
+  l_districts[[7]] <- list(sYear='9596', numYear = 1995, map=l_district_maps[[1]], iMap = 1)
+  l_districts[[8]] <- list(sYear='9596', numYear = 1996, map=l_district_maps[[2]], iMap = 2)
+  
+  l_districts[[9]] <- list(sYear='9798', numYear = 1997, map=l_district_maps[[3]], iMap = 3)
+  l_districts[[10]] <- list(sYear='9798', numYear = 1998, map=l_district_maps[[3]], iMap = 3)
+  
+  l_districts[[11]] <- list(sYear='9900', numYear = 1999, map=l_district_maps[[4]], iMap = 4)
+  l_districts[[12]] <- list(sYear='9900', numYear = 2000, map=l_district_maps[[4]], iMap = 4)
+  
+  l_districts[[13]] <- list(sYear='0102', numYear = 2001, map=l_district_maps[[5]], iMap = 5)
+  l_districts[[14]] <- list(sYear='0102', numYear = 2002, map=l_district_maps[[5]], iMap = 5)
+  
+  l_districts[[15]] <- list(sYear='0304', numYear = 2003, map=l_district_maps[[6]], iMap = 6)
+  l_districts[[16]] <- list(sYear='0304', numYear = 2004, map=l_district_maps[[6]], iMap = 6)
+  
+  l_districts[[17]] <- list(sYear='0506', numYear = 2006, map=l_district_maps[[7]], iMap = 7)
+  
+  l_districts[[18]] <- list(sYear='0708', numYear = 2007, map=l_district_maps[[8]], iMap = 8)
+  l_districts[[19]] <- list(sYear='0708', numYear = 2008, map=l_district_maps[[8]], iMap = 8)
+  
+  l_districts[[20]] <- list(sYear='0910', numYear = 2009, map=l_district_maps[[9]], iMap = 9)
+  l_districts[[21]] <- list(sYear='0910', numYear = 2010, map=l_district_maps[[9]], iMap = 9)
+  
+  l_districts[[22]] <- list(sYear='1112', numYear = 2011, map=l_district_maps[[10]], iMap = 10)
+  l_districts[[23]] <- list(sYear='1112', numYear = 2012, map=l_district_maps[[10]], iMap = 10)
+  
+  l_districts[[24]] <- list(sYear='1314', numYear = 2013, map=l_district_maps[[11]], iMap = 11)
+  l_districts[[25]] <- list(sYear='1314', numYear = 2014, map=l_district_maps[[11]], iMap = 11)
+  
+  names(l_districts) <- sapply(l_districts, '[[', 2)
+  saveRDS(l_districts, file=district_maps_location)
    } else {
     district_maps <- readRDS(district_maps_location)
   }
